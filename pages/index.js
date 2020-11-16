f59a59e20084fa0ac48c86daae2449a4e7cbae47
Helmut Drewes <helmut.drewes@agrista.com>
2020-11-16 19:42:37 +0200

    First commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Nav from '../components/nav'
+
+export default function IndexPage() {
+    return (
+        <div>
+            <Nav />
+            <div className="py-20">
+                <h1 className="text-5xl text-center text-accent-1">
+                    Your partner on your journey to wellness
+                </h1>
+            </div>
+        </div>
+    )
+}
