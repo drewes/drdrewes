77cc9659962b25a0fc36e65db63bc4b2e7452830
Helmut Drewes <helmut.drewes@agrista.com>
2021-05-10 10:26:14 +0200

    Update feature list

--- components/blocks/FeatureList/FeatureList.tsx
+++ components/blocks/FeatureList/FeatureList.tsx
@@ -32,16 +32,17 @@ export default function Example() {
     <div className="bg-white">
       <div className="px-4 py-16 mx-auto max-w-7xl sm:px-6 lg:py-24 lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
         <div>
-          <h2 className="text-base font-semibold tracking-wide uppercase text-pink-dark">Optimise your health</h2>
-          <p className="mt-2 text-3xl font-extrabold text-gray-900 font-display">Targeted interventions that impact your well-being</p>
+          <h2 className="text-base font-semibold tracking-wide uppercase text-pink-dark">
+            Optimise your health
+          </h2>
+          <p className="mt-2 text-3xl font-extrabold text-gray-900 font-display">
+            Targeted interventions that impact your well-being
+          </p>
           <p className="mt-4 text-lg text-gray-500">
-            We strive to create an environment of acceptance,
-            hope and healing. We believe optimal health is achieved through
-            helping restore the functional pathways of the body and through
-            preventing disease by correcting deficiencies at the earliest
-            opportunity. We believe optimal health is possible for every
-            atient regardless of their medical condition.
-            </p>
+            We offer a range of targeted treatments. Our clinicians
+            will assess you and based on your profile suggest applicable
+            options.
+          </p>
         </div>
         <div className="mt-12 lg:mt-0 lg:col-span-2">
           <dl className="space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:grid-rows-3 sm:grid-flow-col sm:gap-x-6 sm:gap-y-10 lg:gap-x-8">
